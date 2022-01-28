4f46ff32c41f1706ccb144d750b28dc8c908059b
JulianeFerreira <juliane_ferreiramelo@hotmail.com>
2022-01-28 16:17:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+var btnCriptografar = document.querySelector("#btn-cripto");
+var btnDescriptografar = document.querySelector("#btn-descripto");
+var btnCopiar = document.querySelector("#btn-copy");
+
+var txtMensagem = document.querySelector("#msg");
+var txtEntrada = document.querySelector("#input-texto");
+var errorMessage = document.querySelector("#error-message");
+
+
+//Validar letras minusculas e acentos
+function validarMensagem(expressao, traducao) {
+    if (!/^[a-z0-9 ]*$/.test(txtEntrada.value)) {
+        alert('O texto não deve conter: acentuação, caracteres especiais ou letras maiúsculas!');
+        txtEntrada.value = '';
+        return
+    }
+
+    txtMensagem.value = txtEntrada.value.replace(expressao, chave => traducao[chave]);
+    if (txtMensagem.value == txtEntrada.value) {
+        alert("A mensagem não pode ser descriptografada, insira outra mensagem!");
+        txtMensagem.value = "";
+    }
+    txtEntrada.value = '';
+}
+//Validar input vazio para criptografar
+function validarCriptografia() {
+    if (!txtEntrada.value == "") {
+        validarMensagem(/[aeiou]/g, {
+            'a': 'ai',
+            'e': 'enter',
+            'i': 'imes',
+            'o': 'ober',
+            'u': 'ufat'
+        });
+    } else {
+        alert("Favor, inserir texto no campo abaixo!");
+        txtEntrada.focus();
+    }
+}
+//Validar input vazio para descriptografar
+function validarDescriptografar() {
+    if (!txtEntrada.value == "") {
+        validarMensagem(/ai|enter|imes|ober|ufat/g, {
+            'ai': 'a',
+            'enter': 'e',
+            'imes': 'i',
+            'ober': 'o',
+            'ufat': 'u'
+        });
+    } else {
+        alert("Favor, inserir texto no campo abaixo!");
+        txtEntrada.focus();
+    }
+}
+//Copiar
+function copiar() {
+    if (!txtMensagem.value == "") {
+        navigator.clipboard.writeText(txtMensagem.value);
+        alert("Copiado!");
+        txtMensagem.value = "";
+    } else {
+        alert("Antes de copiar deve ser criptografado algo!");
+    }
+}
+
+
+//Criptografar
+btnCriptografar.addEventListener('click', (e) => {
+    e.preventDefault();
+    validarCriptografia();
+})
+//Descriptografar
+btnDescriptografar.addEventListener('click', (e) => {
+    e.preventDefault();
+    validarDescriptografar();
+})
+//Copiar
+btnCopiar.addEventListener('click', function (e) {
+    e.preventDefault();
+    copiar();
+})
